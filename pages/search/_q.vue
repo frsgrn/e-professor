58c483cffc8173a6ae0d4cf5b3d516829261171b
Victor <forsgren.victor@gmail.com>
2020-05-11 23:31:13 +0200

    cleanup och formattering

--- pages/search/_q.vue
+++ pages/search/_q.vue
@@ -3,8 +3,13 @@
     <div class="section">
       <div class="fancy-article-list paper">
         <div class="content">
-            <h4>Showing results for query "{{this.$route.params.q}}"</h4>
-          <article-preview v-for="result in results" :key="result.item._slug" :post="result.item"></article-preview>
+            <div v-if="results.length > 0">
+                <h4>Showing results for query "{{this.$route.params.q}}"</h4>
+                <article-preview v-for="result in results" :key="result.item._slug" :post="result.item"></article-preview>
+            </div>
+            <div v-else>
+                <h4>No results found for query "{{this.$route.params.q}}"</h4>
+            </div>
       </div>
       </div>
     </div>
@@ -14,7 +19,7 @@
 <script>
 import ArticleView from "~/components/ArticleView.vue";
 import ArticlePreview from "~/components/ArticlePreview.vue";
-import Fuse from 'fuse.js'
+import Fuse from "fuse.js";
 
 export default {
   components: {
@@ -22,16 +27,16 @@ export default {
     ArticlePreview
   },
   async asyncData({ params, app, payload, route, store, error }) {
-      if(!params.q) {
-          return error({ statusCode: 404, message: 'No query' })
-      }
-    const options = {
-        includeScore: true,
-        keys: ['title', 'description', "body", "author", "subject", "date"]
+    if (!params.q) {
+      return error({ statusCode: 404, message: "No query" });
     }
-    const fuse = new Fuse(store.state.blogPosts, options)
-    const result = fuse.search(params.q)
-    return {results: result.sort((a, b) => (a.score < b.score) ? 1 : -1)}
+    const options = {
+      includeScore: true,
+      keys: ["title", "description", "body", "author", "subject", "date"]
+    };
+    const fuse = new Fuse(store.state.blogPosts, options);
+    const result = fuse.search(params.q);
+    return { results: result.sort((a, b) => (a.score < b.score ? 1 : -1)) };
   }
 };
 </script>
