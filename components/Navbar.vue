be3aaaf0ba4e44a1be35428392073c68dc3d5312
Victor <forsgren.victor@gmail.com>
2020-05-12 00:25:54 +0200

    polish text

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -11,8 +11,8 @@
       <!-- Right aligned nav items -->
       <b-navbar-nav class="ml-auto">
         <b-nav-form>
-          <b-form-input size="sm" class="mr-sm-2" placeholder="Search" v-model="searchQuery"></b-form-input>
-          <b-button size="sm" class="my-2 my-sm-0" type="submit" @click.stop.prevent="submit()">Search</b-button>
+          <b-form-input size="sm" class="mr-sm-2" placeholder="Search for content" v-model="searchQuery" style="border: none; color: black;"></b-form-input>
+          <b-button size="sm" class="my-2 my-sm-0" type="submit" @click.stop.prevent="submit()" style="background-color: #341f97; border: none;">Search</b-button>
         </b-nav-form>
       </b-navbar-nav>
     </b-collapse>
