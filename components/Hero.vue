e76a88be753237e082d674f08655398f91751dc4
Victor <forsgren.victor@gmail.com>
2020-05-12 23:23:46 +0200

    Image & hero

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
     <div class="hero" style="border-bottom: 2px solid #eee; padding-bottom: 10px; margin-bottom: 20px;">
+      <div style="display: inline;">
+            <div class="profile" style="float: left;"></div>
+            <div class="information">
+              <h4 style="margin: 0px;">Ossi Pesämaa</h4>
+              <p>Associate professor at Luleå University of Technology in Sweden</p>
+            </div>
+      </div>
       <h2>{{subheadline}}</h2>
     </div>
   </div>
@@ -14,4 +21,23 @@ export default {
 };
 </script>
 <style>
+
+.hero .profile {
+  width: 50px;
+  height: 50px;
+  background-image: url("/profile.jpeg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  border-radius: 100%;
+  margin-right: 20px;
+}
+
+@media screen and (max-width: 800px) {
+  .profile {
+    float: none !important;
+    height: 600px;
+    margin-bottom: 15px;
+  }
+}
+
 </style>
