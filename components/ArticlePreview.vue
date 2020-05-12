e9e198601090c5cf0f352f48f44f1b94737942ee
Victor <forsgren.victor@gmail.com>
2020-05-12 20:43:33 +0200

    mer konsist titel och datum för preview

--- components/ArticlePreview.vue
+++ components/ArticlePreview.vue
@@ -5,10 +5,8 @@
           <p><b>{{post.title}}</b></p>
           <p>{{post.subheading}}</p>
           <div style="color: #00000090;" v-html="$md.set({ html: true, breaks: false }).renderInline(post.body)" class="body-preview"></div>
-          <b-row style="padding: 0px; margin: 0px;">
-            <b-col style="padding: 0px;"><p style="font-weight: bold; border-bottom: 2px solid black; display: inline;">Read more</p></b-col>
-            <b-col style="text-align: right; padding: 0px;"><p style="display: inline;">{{post.date}}, {{post.author}}</p></b-col>
-          </b-row>
+          <p>{{post.date}}, {{post.author}}</p>
+            <p style="font-weight: bold; border-bottom: 2px solid black; display: inline;">Read more</p>
     </div>
     </nuxt-link>
   </div>
