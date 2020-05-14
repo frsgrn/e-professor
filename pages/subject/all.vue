23c41339a158e3e861169d34b280e6e7b8ef8e7a
Victor <forsgren.victor@gmail.com>
2020-05-14 07:44:52 +0200

    Show all articles

---
+++ pages/subject/all.vue
@@ -0,0 +1,26 @@
+<template lang="html">
+  <b-container>
+  <p><nuxt-link to="/">Collections</nuxt-link> <i class="fas fa-angle-right"></i> all</p>
+    <div class="section">
+      <div class="fancy-article-list paper">
+        <div class="content">
+          <h4>All articles</h4>
+          <p>All articles in chronological order.</p>
+          <article-preview v-for="post in this.$store.state.blogPosts" :key="post._slug" :post="post"></article-preview>
+      </div>
+      </div>
+    </div>
+  </b-container>
+</template>
+
+<script>
+import ArticleView from "~/components/ArticleView.vue";
+import ArticlePreview from "~/components/ArticlePreview.vue";
+
+export default {
+  components: {
+    ArticleView,
+    ArticlePreview
+  }
+};
+</script>
