d8f97412288d8fe4a4bb4ec992a8e0b67f149ff1
Victor <forsgren.victor@gmail.com>
2020-05-15 02:31:36 +0200

    Update all.vue

--- pages/subject/all.vue
+++ pages/subject/all.vue
@@ -2,7 +2,7 @@
   <b-container>
   <navigation-bar :routes="[{name: 'articles'}, {name: 'all'}]"></navigation-bar>
     <div class="section">
-      <article-preview-list :posts="this.$store.state.blogPosts" title="All articles" description="The entire collection of articles, sorted chronological order."></article-preview-list>
+      <article-preview-list :posts="this.$store.state.blogPosts" title="All articles" description="The entire collection of articles, sorted in reverse chronological order."></article-preview-list>
     </div>
   </b-container>
 </template>
