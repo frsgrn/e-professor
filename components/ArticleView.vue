2173f3106b6c2baf2f399bdedb7279e1dd82a0ca
Victor <forsgren.victor@gmail.com>
2020-05-15 11:09:36 +0200

    Update ArticleView.vue

--- components/ArticleView.vue
+++ components/ArticleView.vue
@@ -4,7 +4,7 @@
         <h1 style="font-weight: bold; margin: 0px;">{{post.title}}</h1>
         <h2>{{post.subheading}}</h2>
     <div v-if="post.youtubeUrl && post.youtubeUrl != ''">
-        <b-row>
+        <b-row class="section">
         <b-col cols="12" style="text-align: center; margin: auto;">
             <div style="padding-bottom: 56.25%; text-align: center;">
             <iframe style="width:100%;height:100%;position:absolute;left:0px;top:0px;" class="video-embed" width="100%" height="100%" :src="post.youtubeUrl.replace('watch?v=', 'embed/')" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
