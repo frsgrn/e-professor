0476ce356601fbd9c2ebbbdd1054671f9ab218a7
Victor <forsgren.victor@gmail.com>
2020-05-17 09:18:52 +0200

    Create error.vue

---
+++ layouts/error.vue
@@ -0,0 +1,13 @@
+<template>
+  <b-container>
+    <h1 v-if="error.statusCode === 404">Page not found, {{error.statusCode}}</h1>
+    <h1 v-else>An error occurred, {{error.statusCode}}</h1>
+    <p><nuxt-link to="/">Return to {{this.$store.state.siteInfo.sitename}}</nuxt-link></p>
+  </b-container>
+</template>
+
+<script>
+export default {
+  props: ['error']
+}
+</script>
