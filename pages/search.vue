c08ea6526276dedd60c17c1187eb0d8ea4cdeafd
Victor <forsgren.victor@gmail.com>
2020-05-20 13:36:51 +0200

    Update search.vue

--- pages/search.vue
+++ pages/search.vue
@@ -23,35 +23,34 @@ export default {
     lastBlogs() {
       return this.$store.getters.lastBlogs(3);
     },
-    /*results() {
+    results() {
       const options = {
         includeScore: true,
         keys: ["title", "description", "body", "author", "subject", "date"]
       };
       const fuse = new Fuse(this.$store.getters.getBlogPosts(), options);
       return fuse.search(this.$route.query.q).sort((a, b) => (a.score > b.score ? 1 : -1));
-    },*/
+    },
     titleMessage() {
-      if (this.results.length > 0) return this.$L("SEARCH_TITLE") + " \"" + this.$route.query.q + "\""
-      else return this.$L("SEARCH_TITLE_NO_RESULTS") + " \"" + this.$route.query.q + "\""
+      if (this.results.length > 0)
+        return this.$L("SEARCH_TITLE") + ' "' + this.$route.query.q + '"';
+      else
+        return (
+          this.$L("SEARCH_TITLE_NO_RESULTS") + ' "' + this.$route.query.q + '"'
+        );
     },
     recentMessage() {
-      return this.$L('SEARCH_SHOW_RECENTS')
+      return this.$L("SEARCH_SHOW_RECENTS");
     }
   },
   watchQuery: true,
-  async asyncData({ params, app, payload, route, store, error, query, redirect }) {
-    console.log(route)
-    console.log(query)
+  async asyncData({
+    query,
+    redirect
+  }) {
     if (!query.q) {
       return redirect("/");
     }
-    const options = {
-        includeScore: true,
-        keys: ["title", "description", "body", "author", "subject", "date"]
-      };
-      const fuse = new Fuse(store.getters.getBlogPosts(), options);
-      return {results: fuse.search(query.q).sort((a, b) => (a.score > b.score ? 1 : -1))}
   }
 };
 </script>
